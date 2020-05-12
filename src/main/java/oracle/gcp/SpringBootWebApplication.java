12a30dfcffea6598cac603dc458a732b461c9e4d
Vaishali <vaishali.verma@idc.bedbath.com>
2020-05-12 17:14:08 +0530

    Added / endpoint to test

--- src/main/java/oracle/gcp/SpringBootWebApplication.java
+++ src/main/java/oracle/gcp/SpringBootWebApplication.java
@@ -1,12 +1,34 @@
 package oracle.gcp;
 
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootWebApplication {
-
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+    
+    @GetMapping("/")
+    public String hello() {
+    	 System.out.println("Trying to connect to DB");
+    	 try {
+ 			System.err.println(jdbcTemplate.getDataSource().getConnection());
+ 		} catch (SQLException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		}
+    	 return "Connected";
     }
 }
